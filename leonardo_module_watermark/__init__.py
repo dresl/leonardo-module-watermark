ea30e9550eee5efbe4e0bd3395b0a855596e4399
dresl <nikicresl@gmail.com>
2018-08-02 18:47:48 +0200

    Fix module name

--- leonardo_module_watermark/__init__.py
+++ leonardo_module_watermark/__init__.py
@@ -2,11 +2,11 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
-default_app_config = 'leonardo_watermark.Config'
+default_app_config = 'leonardo_module_watermark.Config'
 
 LEONARDO_OPTGROUP = 'Watermark'
-LEONARDO_APPS = ['leonardo_watermark', 'watermark']
+LEONARDO_APPS = ['leonardo_module_watermark', 'watermark']
 
 class Config(AppConfig):
-    name = 'leonardo_watermark'
+    name = 'leonardo_module_watermark'
     verbose_name = _("Watermark")
